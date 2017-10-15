26c58d01387b421cd11ab4747e5b4fe7cccfae1d
evenIfAlsoGo <2624095175@qq.com>
2017-10-15 17:01:23 +0800

    js001

---
+++ README.md
@@ -0,0 +1,2 @@
+原生 JavaScript 练习网站，目前仿造 mhero.com 进行自我学习
+
--- js/js001.js
+++ js/js001.js
@@ -1,38 +1,72 @@
 window.onload=function () {
-	document.getElementById("js001_02").style.display = "none";
-	document.getElementById("js001_04").style.display = "none";
-	document.getElementById("js001_05").style.display = "none";
-	document.getElementById("js001_07").style.display = "none";
+	//模块1 说明事件
+	var oDiv1_btn_alert = document.getElementById("div1_btn_alert");
+	oDiv1_btn_alert.onclick = function () {
+		alert("弹出窗口");
+	}
+	
+	//模块2 移入移出
+	var oDiv2_p_showBtn = document.getElementById("div2_p_showBtn");
+	var oDiv2_btn_hide = document.getElementById("div2_btn_hide");
+	oDiv2_btn_hide.style.display = "none"; //初始化 隐藏按钮
+	oDiv2_p_showBtn.onmousemove = function  (){
+		oDiv2_btn_hide.style.display  ='block';
+	};
+	oDiv2_p_showBtn.onmouseout = function  (){
+		oDiv2_btn_hide.style.display  ='none';
+	};
+	
+	//模块3 函数 鼠标经过这个 div 执行变窄变高变色 移出再恢复
+	var oDiv3_div_changeMore = document.getElementById("div3_div_changeMore");
+	const oDiv3_div_height = oDiv3_div_changeMore.style.height;
+	const oDiv3_div_width = oDiv3_div_changeMore.style.width;
+	const oDiv3_div_bg = oDiv3_div_changeMore.style.background;
+	oDiv3_div_changeMore.onmousemove = function () {
+		oDiv3_div_changeMore.style.background = 'red';
+		oDiv3_div_changeMore.style.height = '200px';
+		oDiv3_div_changeMore.style.width = '80%';
+	}
+	oDiv3_div_changeMore.onmouseout = function () {
+		oDiv3_div_changeMore.style.background = oDiv3_div_bg;
+		oDiv3_div_changeMore.style.height = oDiv3_div_height;
+		oDiv3_div_changeMore.style.width = oDiv3_div_width;
+	}
+	
+	//模块4 鼠标经过就显示弹出层 (函数、变量)
+	var oDiv4_a_showDiv = document.getElementById("div4_a_showDiv");
+	var oDiv4_div_hide = document.getElementById("div4_div_hide");
+	oDiv4_div_hide.style.display = "none";
+	oDiv4_a_showDiv.onmousemove = function () {
+		oDiv4_div_hide.style.display  ='block';
+	}
+	oDiv4_a_showDiv.onmouseout = function  (){
+		oDiv4_div_hide.style.display  ='none';
+	};
+	
+	//模块5 鼠标点击就显示弹出层 (函数、变量、判断)
+	var oDiv5_a_showDiv = document.getElementById("div5_a_showDiv");
+	var oDiv5_div_hide = document.getElementById("div5_div_hide");
+	oDiv5_div_hide.style.display = "none";  //初始化 隐藏弹出层
+	oDiv5_a_showDiv.onclick = function () {
+		oDiv5_div_hide.style.display = oDiv5_div_hide.style.display == 'none'?'block':'none';
+	}
+	
+	//模块6 网页换肤
+	var oDiv6_btn_changeBgcToPink = document.getElementById("div6_btn_changeBgcToPink");
+	var oDiv6_btn_changeBgcToYellow = document.getElementById("div6_btn_changeBgcToYellow");
+	oDiv6_btn_changeBgcToPink.onclick = function () {
+		document.body.style.backgroundColor= "pink";
+	}
+	oDiv6_btn_changeBgcToYellow.onclick = function () {
+		document.body.style.backgroundColor= "yellow";
+	}
+	
+	//模块7 显示隐藏
+	var oDiv7_btn_showBtn = document.getElementById("div7_btn_showBtn");
+	var oDiv7_btn_hide = document.getElementById("div7_btn_hide");
+	oDiv7_btn_hide.style.display = 'none';  //初始化 隐藏按钮
+	oDiv7_btn_showBtn.onclick = function () {
+		oDiv7_btn_hide.style.display = oDiv7_btn_hide.style.display == 'none'?'block':'none';
+	}
 }
-function js001_01() {
-	alert("弹出窗口");
-}
-function js001_02() {
-	var div = document.getElementById('js001_02').style;
-	div.display = div.display == 'none'?'block':'none';
-}
-function js001_03() {
-	var div = document.getElementById('js001_03').style;
-	var bgc = document.body.style.backgroundColor;
-	div.background = div.background=='red'?bgc:'red';
-	div.color = div.color=='white'?'black':'white';
-}
-function js001_04() {
-	var div = document.getElementById('js001_04').style;
-	div.display = div.display == 'none'?'block':'none';
-}
-function js001_05() {
-	var div = document.getElementById('js001_05').style;
-	div.display = div.display == 'none'?'block':'none';
-}
-function js001_06_01() {
-	document.body.style.backgroundColor= "#ff9999";
-}
-function js001_06_02() {
-	document.body.style.backgroundColor= "yellow";
-}
-function js001_07() {
-	var div = document.getElementById('js001_07').style;
-	div.background = "red";
-	div.display = div.display == 'none'?'block':'none';
-}
+
