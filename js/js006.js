aec581cff73b8e54eaff8d8360b1c647ee958075
evenIfAlsoGo <2624095175@qq.com>
2018-02-18 23:42:53 +0800

    update swiper
    增加小圆点和自动翻页功能

--- js/js006.js
+++ js/js006.js
@@ -1,6 +1,9 @@
 window.onload = function () {
-	//slide 数量
+	/*模块1 简易*/
 	var slides = document.getElementById("swiper").getElementsByTagName("div")
+	for(var i=0;i<slides.length;i++) {
+		slides[i].style.display = "none"
+	}
 	slides[0].style.display = "block"
 	slides.show = 0
 	document.getElementById("next").onclick = function(){
@@ -13,4 +16,37 @@ window.onload = function () {
 		slides.show = (slides.show - 1 + slides.length) % slides.length
 		slides[slides.show].style.display = "block"
 	}
+	/*模块2 增加了小圆点，和自动翻页*/
+	var slides2 = document.getElementById("swiper2").getElementsByTagName("div")
+	var circle = document.getElementById("circle")	
+	for(var i=0;i<slides2.length;i++) {
+		slides2[i].style.display = "none"
+		circle.innerHTML = circle.innerHTML + "<span style='font-size:30px'>·</span>"
+	}
+	var circles = circle.getElementsByTagName("span")
+	slides2[0].style.display = "block"
+	slides2.show = 0	
+	circles[slides2.show].style.color = "red"
+	function pageTurn(direction) {
+		slides2[slides2.show].style.display = "none"
+		circles[slides2.show].style.color = "black"
+		if(direction=="next") {
+			slides2.show = (slides2.show + 1) % slides2.length;
+		} else if(direction=="prev") {
+			slides2.show = (slides2.show - 1 + slides2.length) % slides2.length
+		}
+		slides2[slides2.show].style.display = "block"
+		circles[slides2.show].style.color = "red"
+	}
+	var timer = function(){
+		pageTurn("next")
+	}
+	window.setInterval(timer,2000);
+	document.getElementById("next2").onclick = function(){
+		pageTurn("next")
+	}
+	document.getElementById("prev2").onclick = function(){
+		pageTurn("prev")
+	}
+	
 }
